cc7b867cbc9ec6a949dbf8ffb86bd9a1cd591219
fjpging8908 <fjpging@gmail.com>
2019-05-19 14:30:42 -0500

    Ajuste environment.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,7 @@
 
 export const environment = {
   production: false,
-  //apiUrl: 'https://agiles-grupo1-sisred-back.herokuapp.com/api/'
-   apiUrl: 'http://127.0.0.1:8000/api/'
+  apiUrl: 'https://agiles-grupo1-sisred-back.herokuapp.com/api/'
 };
 
 /*
